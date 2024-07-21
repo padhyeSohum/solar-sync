55115553f4677ed01c75e41a9f033eb508c8e5f8
paninizer <101371138+paninizer@users.noreply.github.com>
2024-07-21 12:57:21 -0400

    simple navbar

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,6 @@
 import { Inter } from "next/font/google";
 import LightDarkButton from "@/components/light-dark-button";
+import { AppBar, HeaderOffset, Logo, Toolbar } from "./styled";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -8,7 +9,45 @@ export default function Home() {
         <main
             className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}
         >
-            <LightDarkButton />
+
+          <div>
+            <AppBar>
+                <Toolbar component="div" sx={{ display: { xs: 'none', sm: 'flex' } }}>
+                    <Logo className = "py-4">Solar Sync</Logo>
+                    <nav className="relative select-none lg:flex lg:items-stretch w-full py-4">
+                    <div className="flex flex-no-shrink items-stretch h-12 justify-normal">
+                        <a href="#"
+                        className="group rounded-lg border border-transparent px-5 py-2 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30 justify-items-center"
+                        target="_blank"
+                        rel="noopener noreferrer"
+                        >
+                            <h2 className={`mb-3 text-2xl font-semibold`}>
+                                Section 1{" "}
+                            </h2>
+                        </a>
+
+                        <a href="#"
+                        className="group rounded-lg border border-transparent px-5 py-2 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30 justify-items-center margin-left-7"
+                        target="_blank"
+                        rel="noopener noreferrer"
+                        >
+                            <h2 className={`mb-3 text-2xl font-semibold`}>
+                                Section 2{" "}
+                            </h2>
+                        </a>
+                    </div>
+                    </nav>
+
+                    <LightDarkButton />
+                </Toolbar>
+            </AppBar>
+            <HeaderOffset />
+          </div>
+
+          
+          <div>
+            <h1><span className="gradient-underline">Cool Slogan Here!</span></h1>
+          </div>
 
             <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
                 <a
